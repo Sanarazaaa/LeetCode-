56d56853868946ee76702c3940a4b1fa401493ae
Sana <165957167+Sanarazaaa@users.noreply.github.com>
2024-10-13 13:17:42 +0500

    Time: 51 ms (31.07%) | Memory: 16.7 MB (14.59%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,3 +1,10 @@
+# the idea is to subtract the previous with the current (e.g: 9 --> X - I = IX = 9)
+# when we want to substract something, then we need to have one value in -1; this is to ensure that we can subtract.
+# s = 10 ; i = 0  ; line # 15 -> 0 < 10; line 16 --> if 0  
+# Iteration with i = 0:
+# If I just wrote roman_values[s[i]] < roman_values[s[i + 1]] without the length check:
+# It would attempt to access s[1], which does not exist. Result? IndexError.
+
 class Solution:
     def romanToInt(self, s: str) -> int:
         roman_values = {
@@ -5,9 +12,9 @@ class Solution:
     'C': 100, 'D': 500, 'M': 1000
 }
         total = 0
-        i = 0
+        i = 0 
         
-        while i < len(s):
+        while i < len(s): # this condition is essential because we dont want to have an index error. for instance, s = IX, here, len = 2, so i = 0, and i = 1 should be used. if we use don't mention this relation, then i would then become (2+1) which is something that aint present here (next line)
             if i < len(s) - 1 and roman_values[s[i]] < roman_values[s[i + 1]]:
                 total -= roman_values[s[i]]  # Subtract current value
             else:
