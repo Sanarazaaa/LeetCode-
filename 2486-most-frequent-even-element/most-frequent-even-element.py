cd779860738c485e0a91d8612bc5afab37ac1f34
Sana Raza <165957167+Sanarazaaa@users.noreply.github.com>
2024-12-21 00:37:26 +0500

    Time: 11 ms (97.30%) | Memory: 18.4 MB (5.57%) - LeetSync

--- 2486-most-frequent-even-element/most-frequent-even-element.py
+++ 2486-most-frequent-even-element/most-frequent-even-element.py
@@ -1,14 +1,15 @@
 class Solution:
     def mostFrequentEven(self, nums: List[int]) -> int:
-        # Create a Counter for even numbers
-        even_count = Counter()
-        for index, number in enumerate(nums):  # Using enumerate
-            if number % 2 == 0:  # Check if the number is even
-                even_count[number] += 1
+        d ={}
+        for i in nums:
+            if i%2==0:
+                d[i] = d.get(i,0)+1
         
-        if not even_count:  # If no even numbers, return -1
+        if d=={}:
             return -1
-        
-        # Find the most frequent even number
-        most_frequent = sorted(even_count.items(), key=lambda x: (-x[1], x[0]))
-        return most_frequent[0][0]
+        x= max(d.values())
+        mn = float('inf')
+        for k,v in d.items():
+            if v==x:
+                mn = min(k,mn)
+        return mn
