03bfbb30aa7108e48480f51c54308af566cef513
Sana Raza <165957167+Sanarazaaa@users.noreply.github.com>
2025-01-13 10:37:32 +0300

    Time: 1790 ms (16.14%) | Memory: 18.4 MB (46.72%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):  # Loop through the list using indices
+            for j in range(i + 1, len(nums)):  # Check pairs without repetition
+                if nums[i] + nums[j] == target:
+                    return [i, j]  # Return the indices of the two numbers
+        return []  # Return an empty list if no solution is found
