6f046bd5caac08e6e5433fa08bfa02eceb09d505
Sana Raza <165957167+Sanarazaaa@users.noreply.github.com>
2025-03-31 12:52:00 +0500

    Time: 11 ms (55.25%) | Memory: 36.2 MB (90.10%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.py
+++ 2634-minimum-common-value/minimum-common-value.py
@@ -1,6 +1,5 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        res = []
         p1, p2 = 0,0
         while p1 < len(nums1) and p2 < len(nums2):
             if nums1[p1] == nums2 [p2]:
