452c60f7e36d3ed53de7f6396f1808d9e67eddd6
Sana Raza <165957167+Sanarazaaa@users.noreply.github.com>
2025-04-06 02:52:51 +0500

    Time: 11 ms (72.53%) | Memory: 17.8 MB (86.46%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,16 +1,17 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        # Check if lengths are the same
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        
-        # Compare the frequency counts of characters in both strings
-        return Counter(s) == Counter(t)
-
-        
-        
+        count_s = {}
+        count_t = {}
+        for char in s:
+            if char in count_s:
+                count_s[char] += 1
+            else:
+                count_s[char] = 1
+        for char in t:
+            if char in count_t:
+                count_t[char] += 1
+            else:
+                count_t[char] = 1
+        return count_s == count_t
