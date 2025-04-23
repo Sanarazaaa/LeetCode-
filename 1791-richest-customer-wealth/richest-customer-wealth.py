eab4d2dea6b85ecb023eb60c952b4dd766c873c2
Sana Raza <165957167+Sanarazaaa@users.noreply.github.com>
2025-04-23 16:38:00 +0500

    Time: 0 ms (100.00%) | Memory: 18.1 MB (14.16%) - LeetSync

---
+++ 1791-richest-customer-wealth/richest-customer-wealth.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        max_row_sum = sum(accounts[0]) #         max_row = accounts[0] -- this is how to get the first row
+        for i in range(1, len(accounts)):
+            current_sum = sum(accounts[i])
+            if max_row_sum < current_sum:
+                max_row_sum = current_sum
+        return max_row_sum
