c69fe3621f48544b981f39ce6a3dd9174568432b
Sana Raza <165957167+Sanarazaaa@users.noreply.github.com>
2025-04-26 14:12:41 +0500

    Time: 7 ms (89.45%) | Memory: 22.9 MB (93.22%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.py
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.py
@@ -1,11 +1,11 @@
 # 1d list is a simple list. not a list inside a list
 # 0-indexed means: First element is at index 0, second at index 1, third at index 2, etc.
 class Solution:
-    def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
+    def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]: # the function will take 3 inupts
         result = []
         if m*n == len(original):
-            for i in range(0,len(original),n):
-                result.append(original[i:i+n])
+            for i in range(0,len(original),n): #i will go from 0 to len(original), jumping n steps at a time.
+                result.append(original[i:i+n]) #just slicing
         return result
 
 
