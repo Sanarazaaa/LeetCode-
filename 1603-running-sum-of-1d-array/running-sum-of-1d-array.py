0a337ac8dd9234110ef59e2f17727da1b082d701
Sana Raza <165957167+Sanarazaaa@users.noreply.github.com>
2025-04-26 13:13:07 +0500

    Time: 0 ms (100.00%) | Memory: 18 MB (24.94%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
@@ -1,7 +1,8 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        num = [0] *len(nums) # this will give me [0,0,0,0]. this is used to create  alist of zeros
-        num[0] = nums[0] # this will give me [1]
-        for i in range(1,len(nums)):
-            num[i] = nums[i] + num [i-1]
-        return num
+        c_sum= 0
+        result = []
+        for i in nums:
+            c_sum += i
+            result.append(c_sum)
+        return result
