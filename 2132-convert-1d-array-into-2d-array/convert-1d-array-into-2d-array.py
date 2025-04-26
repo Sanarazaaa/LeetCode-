8487e6d406bda88cbf926e38b08597421a6512f4
Sana Raza <165957167+Sanarazaaa@users.noreply.github.com>
2025-04-26 13:51:36 +0500

    Time: 11 ms (67.28%) | Memory: 23 MB (74.44%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.py
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.py
@@ -1,10 +1,11 @@
+# 1d list is a simple list. not a list inside a list
+# 0-indexed means: First element is at index 0, second at index 1, third at index 2, etc.
 class Solution:
     def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
-        new = []
-        for i in range(0,len(original),n):
-            if len(original) == m*n:
-                new.append(original[i:i+n])
- # i have to append the sliced one not the original one           new.append(original)  
-            else:
-                return []
-        return new
+        result = []
+        if m*n == len(original):
+            for i in range(0,len(original),n):
+                result.append(original[i:i+n])
+        return result
+
+
