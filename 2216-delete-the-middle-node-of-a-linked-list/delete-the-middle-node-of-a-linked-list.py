32be4f8d9607f7030d3fe6b7f44e07c2676b5fc9
Sana Raza <165957167+Sanarazaaa@users.noreply.github.com>
2025-05-09 13:04:10 +0500

    Time: 56 ms (56.38%) | Memory: 49 MB (49.83%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.py
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.py
@@ -5,16 +5,12 @@
 #         self.next = next
 class Solution:
     def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # if there would be 0 or 1 node, there wont be any middle node
-        if not head or not head.next:
+        if head is None or head.next is None: # if there is no head and there is no head.next then return None
             return None
-        slow = head
-        fast = head
-        prev = None
-        while fast and fast.next:
-            prev = slow
-            slow = slow.next
-            fast = fast.next.next
-        
-        prev.next = slow.next
+        slow = head 
+        fast = head.next.next if head.next else None # why fast = head.next.next? well, we have to start head.next.next first so that slow can reach to a point that is one less than the middle
+        while fast and fast.next: # in case there is still a value in fast and fast.next
+            slow = slow.next # move slow pointer with one pointer
+            fast = fast.next.next # move fast pointer with two
+        slow.next = slow.next.next # when you reach at the point before the middle point then connect it with the node after middle one. why are we stopping before middle one? so that the link isnt lost.
         return head
